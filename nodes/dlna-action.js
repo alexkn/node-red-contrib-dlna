ff2c8855f231d54fcfd2a907b0c5122668e50456
Alexander Knöbel <alex-git@knoebel.info>
2020-02-15 20:50:56 +0100

    Add action node

---
+++ nodes/dlna-action.js
@@ -0,0 +1,37 @@
+const MediaRendererClient = require("upnp-mediarenderer-client");
+
+module.exports = function(RED) {
+    function DlnaAction(config) {
+        RED.nodes.createNode(this,config);
+
+        this.on("input", (msg, send, done) => {
+            let client = new MediaRendererClient(msg.deviceUrl);
+
+            let callback = (err, result) => {
+                if(err) {
+                    done(err);
+                } else {
+                    msg.payload = result;
+                    send(msg);
+                    done();
+                }
+            };
+
+            switch(msg.payload.action) {
+                case "play":
+                    client.play(callback);
+                    break;
+                case "pause":
+                    client.pause(callback);
+                    break;
+                case "stop":
+                    client.stop(callback);
+                    break;
+                case "load":
+                    client.load(msg.payload.url, msg.payload.options, callback);
+            }
+        });
+    }
+
+    RED.nodes.registerType("dlna-action", DlnaAction);
+};
