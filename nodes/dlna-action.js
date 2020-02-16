7a7f6f0456f5b11e3bb88f882937a324cd47888f
Alexander Knöbel <alex-git@knoebel.info>
2020-02-16 21:03:12 +0100

    add device discovery

--- nodes/dlna-action.js
+++ nodes/dlna-action.js
@@ -1,13 +1,39 @@
 const MediaRendererClient = require("upnp-mediarenderer-client");
+const Ssdp = require("../lib/Ssdp");
+
+/**
+ *
+ * @param {string} device
+ */
+const findDeviceUrl = (device) => {
+    if(device.startsWith("http://") || device.startsWith("https://")) {
+        return device;
+    }
+    let devices = Ssdp.Devices();
+
+    // find by usn
+    if(devices[device]) {
+        return devices[device].deviceUrl;
+    }
+
+    // find by friendly name
+    for (var usn in devices) {
+        let d = devices[usn];
+        if(d.name === device) {
+            return d.deviceUrl;
+        }
+    }
+};
 
 module.exports = function(RED) {
     function DlnaAction(config) {
         RED.nodes.createNode(this,config);
 
-        this.deviceUrl = config.deviceUrl;
+        this.device = config.device;
 
         this.on("input", (msg, send, done) => {
-            let client = new MediaRendererClient(msg.deviceUrl || this.deviceUrl);
+            let deviceUrl = findDeviceUrl(msg.device || this.device);
+            let client = new MediaRendererClient(deviceUrl);
 
             let callback = (err, result) => {
                 if(err) {
@@ -36,4 +62,8 @@ module.exports = function(RED) {
     }
 
     RED.nodes.registerType("dlna-action", DlnaAction);
+
+    RED.httpAdmin.get("/dlna/devices", (req, res) => {
+        res.send(Ssdp.Devices());
+    });
 };
